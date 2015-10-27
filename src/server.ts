13d859b4c61930eceb4a1f192949a8d7b8243244
Matt Poegel <mpoegel@comcast.net>
2015-10-27 09:49:54 -0400

    added config.json to specify port

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import controllers = require('./controllers/index');
 import lessMiddleware = require('less-middleware');
 var app = express();
 
+var config = require('./config.json');
+
 app.use(lessMiddleware(
 	__dirname + '/public',
 	{ force: true }
@@ -14,7 +16,7 @@ app.use('/', express.static(__dirname + '/public'));
 
 app.use('/', controllers);
 
-let server = app.listen(8000, () => {
+let server = app.listen(config.port, () => {
 	let host = server.address().address,
 		port = server.address().port;
 	console.log('App running at http://%s:%s', host, port);
